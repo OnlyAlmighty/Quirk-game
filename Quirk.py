b9eab4b59632011d6d591fbaf3c8e0c849f829bb
OnlyAlmighty <121882741+OnlyAlmighty@users.noreply.github.com>
2023-01-04 23:32:13 +0530

    Quirk
    This game is made using by some command i hope you like :D

---
+++ Quirk.py
@@ -0,0 +1,227 @@
+print("\033[31m",'''
+
+████████████████████████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░░░███░░░░░░██░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░██░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░███░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀░░██░░▄▀▄▀░░█
+█░░▄▀░░░░░░▄▀░░███░░▄▀░░██░░▄▀░░█░░░░▄▀░░░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░██░░▄▀░░░░█
+█░░▄▀░░██░░▄▀░░███░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░████░░▄▀░░███░░▄▀░░██░░▄▀░░███
+█░░▄▀░░██░░▄▀░░███░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░▄▀░░███
+█░░▄▀░░██░░▄▀░░███░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░███
+█░░▄▀░░██░░▄▀░░███░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░░░░░▄▀░░░░███░░▄▀░░░░░░▄▀░░███
+█░░▄▀░░██░░▄▀░░███░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░█████░░▄▀░░██░░▄▀░░███
+█░░▄▀░░░░░░▄▀░░░░█░░▄▀░░░░░░▄▀░░█░░░░▄▀░░░░█░░▄▀░░██░░▄▀░░░░░░█░░▄▀░░██░░▄▀░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀░░█
+█░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░██░░░░░░░░█
+████████████████████████████████████████████████████████████████████████████████''',"\033[0m")
+print("\033[32m","How this game work:- First you have to enter your game name and a superpower you want than this game provide you a short story related to your superpower.","\033[0m")
+start = input("Ready to enter in the magical world?  ")
+print("1")
+print("2")
+print("3")
+print(" Let's go!")
+name = input("Enter Your username: ")
+print("Choose your power carefully")
+power2 = input(''',Time Travel, ,invisibility, ,Magic : ''')
+print("Oh! i forget to tell you that each power come whit one disadvantage.")
+print(name, "Welocme mate! i am looking for a companion for this journey")
+print("i need your help i can't compelete these tasks")
+print(power2, "Look like your superpower can help me")
+print("You have to look make story and tell the prince so he can free me.")
+print("Go! and make your story comeback!")
+start2 = input("Ready to make a story? ")
+print("1")
+print("2")
+print("3")
+print(" Let's go!")
+if power2 =="Time Travel":
+    print("\033[35m",'''She set the date, place and time. Confidently, she pressed the start button and time traveled in past. She met a guy in alone. She killed him with sharp knife hits with full of anger and returned back.
+
+Next day, again, she entered inside the time machine, this time, she selected another date, but same place and time. More confidently, she pressed the start button and time traveled in past. She met the same guy in alone. Again, she killed him with sharp knife hits with full anger till his last breathe.
+
+After all, she was dying every day and killing her rapist once was not enough!
+
+''',"\033[0m")
+else:
+    print("\033[36m",'''It was the beginning of the pandemic. Youd always wanted to roam these halls alone. As you waited for the dismissal bell to ring that Friday you went over the brilliant plan in your head, reviewing every detail. When the bell finally rang you were out the door in a flash, lugging your extra bag down the hall and out the front door of the school building. You tried to look casual as you made a point to say goodbye to each teacher on duty. You made sure everyone saw you leave the campus. You probably fooled them.
+
+When you were out of sight you turned and made the block, coming to an overgrown vacant lot right behind the campus. This was the infamous lot where the popular kids came to drink and make out. You picked your way through the vines and trash, booze bottles, beer cans, (someone’s sock? Ewww.) piles of leaves, and an extinguished campfire. Your hair got tangled in the branches, but you kept going. You knew there was a brief window when that back door would be open….
+
+You came to the boards that you had loosened earlier and you swung them open. Perfectly selected right behind the dumpster…and yes, you did hear the custodian unloading the trash…and yes, he left the door to the school open…you wondered if you could make it without being seen. You imagined yourself a lizard and skittered into the door and then sidestepped into this classroom. 
+
+Now youre hiding in the closet of your history teacher, Mr. Miller. When you stepped into his classroom you noticed that his belongings were still on his desk, waiting for him to grab them after carpool duty. Before he opened the door you ducked into the closet hoping he wouldn’t notice the gym bag that you left on the floor. 
+
+You hear the slam of the classroom door. Hold your breath and try not to move a muscle, or worse topple that pile of books near your left knee. Yes, that is what is digging into your flesh; the corner of US History from Civil War to Civil Rights book. You can hear him moving stuff in his desk and the door slams again.
+
+You can breathe now. It’s quiet. Listen, the custodians must be finished already. Better text your mom now; get the lies in motion to take your little vacay away from the fray.
+
+You type without looking at the keys:
+
+“spending the nite with Larkin. K?”
+
+You wait for the response you know will come quickly.
+
+“K” 
+
+Thats it. Just “k”
+
+Ok, get out of this stinkin classroom and have some fun! Freedom! This building is vacant for the next two weeks for the pandemic! You are brilliant! What a great plan! Have you ever noticed how shiny the floor is in this hallway? Perfect for winter Olympic curling. You are both athlete and sportscaster:
+
+“And next its the high school nobody from the USA trying for the gold medal. She winds up and flings her backpack! Look at the speed and distance on that backpack, folks! It’s going all the way! A new Olympic backpack curling record set by the nobody from the USA!”
+
+You kick your shoes off, hands in the air in victory. Then you cartwheel down the hall with the unbridled joy of your ten year old self now stuck in this 15 year old body. You land in the lobby.You stretch out on your back on the cold shiny floor. You feel free. 
+
+You look up to the domed ceiling of this enormous space. Quite grand for a high school, everyone thinks. Encircling the curved walls holding up the dome are windows. You notice the blue sky, fluffy clouds, the branch of a nearby tree with its leaves just beginning to come out, that bright lime green that means spring.
+
+Your mind takes you back to the day you became invisible.
+
+You feel invisible at home. Your mother only notices you when she needs you to do her mom work while she is back in her home office drinking.
+
+You do laundry, cook dinner, take care of your younger sister. Your mother only emerges when you retreat to your room to do your homework, chat with your friends, and go to bed. Days go by without even seeing your mother. Your only evidence that she knows you exist are your orders that she shouts through the door.
+
+But the day you became invisible to your friends was the worst. 
+
+It was last Monday in your first period class that you noticed your friends huddled in a corner when you walked in. One of them looked up in your direction, then whispered too loudly, “Look who’s coming!” They all turned their eyes to you just as the bell rang. Then they all quickly looked down and hustled to their seats. That was the last moment that they saw you. When the bell rang, the girls all quickly left and didn’t even look at you or say a word. You walked to your next class . . . alone. You had become invisible.
+
+Lunch had been humiliating. You grabbed your lunch bag from your locker, eager to connect with your friends to find out what was up. You got to the table first; everyone else was buying today, you figured. You craned your neck to see if you could find them standing in line. You saw Sarah. . .Larkin. . . Clara. . . “Ok, they should be over here in a minute,” you thought. Pulling your sandwich out of your bag, you saw Bev sitting with Melinda on the other side of the cafeteria. Then Sarah, Larkin, and Clara paraded over there with their trays. What was up? You all had been sitting at the same table since you were freshmen. The one where invisible you was sitting.
+
+You ate alone in silence with the bustle of the cafeteria all around you. 
+
+You would have felt self-conscious if anyone had looked at you. Not one person did. Your friends never even glanced your way. Soon other kids sat down near you, but didn’t speak or even look at you. That was when you decided that you must have been hit with the spell of invisibility.
+
+So you came up with the plan to spend the next two weeks in the school building while it was closed for quarantine during this pandemic.
+
+You suddenly feel sad again. You know what to do when that feeling starts to seep into you. You jump up and MOVE!
+
+You race over to the gym, flicking on the lights. Running, running, running, around and around. Then you take off to the pool and jump in, clothes and all, and try to swim a lap, but your clothes are weighing you down. You let yourself sink until your lungs burn, then you explode up and gulp the sweet fresh air. 
+
+You see a woman with black hair pacing on the deck of the pool, holding one of the life preservers that hangs on the wall. Before you know it, you get beaned by that life preserver. You take in a mouthful of water. Coughing and sputtering, your head bobs up and down beside the life preserver. You try to get your bearing when you hear a splash. Waves of pool water cover your head. Your legs, already tired from the run and the weight of the wet clothes makes treading water difficult and you can’t tell where the edge of the pool is. You grab the sides of the life preserver and pull your head through just as you are pulled under again. Under the water, you open your eyes to see the terrified face of the woman with the black hair. She is Medusa with her hair fanning away from her face, her hand clenching your shoulder. You desperately try to kick your way to the surface, but the woman keeps pushing you down.
+
+Realizing you must get free, you duck down and under the woman to swim up behind her to the surface. Gasping for air you steady yourself, then pull the flailing, bobbing woman by her hair to the edge of the pool. You lift the woman’s chin up for her so she can get some air. You guide her hand to the edge of the pool so that she can hold on for herself. Then you find the ladder and hoist yourself to the pool deck, collapsing. 
+
+When you come to awareness again it is the sound of drips, then the feel of drips and when you open your eyes, those drips hit your eyeball.
+
+Shielding your eyes you look up.
+
+ “Hey, there,” the dripping woman standing over you says, “You ok?” Drip, drip. It was Medusa, but her black hair is now plastered against her face. You move up to lean on your elbow. “Sure, I’m ok. How about you? Are you OK?”
+
+Medusa straightens up and walks over to the towel lockers. She pulls down two of them and hands one to you. “Yes, I’m ok. Considering I don’t know how to swim. That was terrifying,” she said drying her hair.
+
+You sit up and wipe your face. Even though you’re a little shaky, you make a turban with the towel and stand.
+
+“What are you doing in here?” you ask Medusa.
+
+Medusa laughs. “I should ask the same of you!”
+
+“Well, I am a student of this high school,” you say defiantly.
+
+“Yes, I know, because I am employed by this high school,” Medusa said patiently.
+
+“Are you a security guard?” you ask. You hadn’t counted on an all night security guard.
+
+“Well, you could say that, but I am employed by this school as a custodian. I am here every day in the lobby and in the junior wing,” Medusa explained.
+
+ 
+
+You are a junior. You have never even seen this woman before. At least you don’t remember ever having seen her. You stare at her, trying to remember.
+
+“I clean the women’s restrooms three times each day, empty all trash cans at least once, dust mop the halls twice a day, wet mop the halls at least once each day, and I clean the glass doors. My favorite job is polishing the brass rail around the lobby,” she said proudly.
+
+Bingo! You do remember her. “Oh, yes, I have seen you in the lobby!” 
+
+Medusa seems pleased that you recognized her. She says shyly, “I thought I was invisible to you students.”
+
+You release a nervous little laugh, looking for a way out.
+
+“I am sorry to hear about your friends,” she says, using your name.
+
+Wait, you didn’t tell Medusa what your name was. And how does she know who your friends are?
+
+“Ma’am? I don’t know what you mean,” you say politely. Suddenly you’re very interested in what she has to say.
+
+“Your friends, those girls you hang out with, or did hang out with until the weekend before last,” Medusa said, “You know. I could see that you didn’t hang out with them after that. You probably didn’t want to associate with them after what they did.”
+
+You have no idea what this woman is talking about.
+
+She continues, “It’s not easy losing your friends. I myself had to leave family and friends behind to come work here at this school. It can be quite lonely. I long for the day when I can be with them again,” Medusa said sadly. 
+
+Then she brightens up and continues, “Come on, let’s get dried off and I’ll show you something.” You find some volleyball uniforms and she finds some flip flops and you both leave your own clothes in the dryer.
+
+“What’s your name?” you finally ask her.
+
+“Rhoda,” she says, “it means ‘rose’.”
+
+You walk out to the green space where students eat at picnic tables. She leads the way to a shed in the corner. Behind it there is a storm drain. Rhoda lifts the grate and descends a rough staircase. You follow.
+
+Down you climb, but not into darkness. It is a modest but stylish apartment! “Is this where you live?” you ask.
+
+“Yes,” Rhoda answers humbly.
+
+“It’s really nice,” you say. “Thank you for showing me.”
+
+You realize this is Rhoda’s way of saying, “I won’t tell on you. I have secrets, too.”
+
+You climb back out into the green space. Rhoda explains, “I came to this town from the reservation looking for a job. No one wanted to hire me in the shops, but I did get a custodian position here when it was still under construction. I made friends with the construction workers. I helped clean up the work site, did odd jobs for them, ran errands. They built this out for me with leftover materials so I would have shelter until I started working. It has been a nice home so I just stayed.”
+
+The two of you sit at a picnic table near a koi pond. 
+
+Looking straight into your soul, Rhoda asks point blank, “So, why are you hanging out here, instead of at home where your people love you?”
+
+You sigh. Your story of invisibility is nothing compared with Rhoda’s. So you say, “I don’t know. I just need some space to think. My friends. . . .”
+
+Rhoda said, “You don’t know what happened with your friends, do you?”
+
+You just shake your head.
+
+Rhoda says, “They wanted to do something daring. They didn’t invite you to go because they knew you would not approve. They got into big trouble, too. In a way, by excluding you, they saved you from getting into big trouble as well.”
+
+You’re hurt. “But why didn’t they confide in me afterwards?”
+
+Rhoda says, “I think you already know. They are embarrassed. Because you ignored them, they thought you knew and didn’t want to associate with them.”
+
+You protest with self righteous indignation, “I didn’t ignore them, they ignored me!”
+
+“Perception,” Rhoda says simply.
+
+“What do you mean by that?” you huff, “that my perception is wrong?”
+
+“Perception is neither right or wrong. It is the way you see something. Thinking about it from another person’s viewpoint can help one understand something by widening one’s perception.”
+
+When Rhoda realizes that you still didn’t understand, she continues gently, “Maybe think of perception like this: someone steps on your foot. It hurts. You feel anger, which is a natural response to that. But then the person turns around, not to apologize, just turns. You realize the person is blind. Your perception widens. You understand more. What will your reaction be after you understand more? That is your choice in the situation.”
+
+You think about this for a bit because you can now relate to that way of thinking. You have changed your mind about someone when you knew more about them.
+
+But then you want to know something else. “How do you know about my friends? How do you know more than I do about them?”
+
+Rhoda smiles. You can see it is a smile of someone who knows sadness and has learned to make the best of things just the way they are. You see yourself. 
+
+Rhoda explains, “I am invisible to you children. You do not see me even when I am there, working for you by keeping your school neat and clean, and polished. My work goes unseen unless it is not done. No one notices a clean window pane. But everyone notices the smudges, smears, and dirt.
+
+“Being invisible does give me a chance to hear things that others cannot hear. Even in a group of people chattering, not many ears are listening. Your friends could use a friend right now,” Rhoda said quietly, again, using your name.
+
+You sit there, stunned. You had no idea. You need to think, to process all of this. Rhoda senses this and leaves you to the silence in the green space. The sun goes down bringing darkness.
+
+You sit on the grass and lean back to look up at the stars. Sometimes looking up makes you feel like you can grab those little bits of sparkly glitter in the sky and sprinkle them over the Earth. Not tonight. With your mind opened you felt as small as a speck of sand. Your problems are tiny compared with the problems other people face. Rhoda found strength inside herself to do some amazing things, to make a difference. You now feel like you might have that strength, too. You long for it. 
+
+You jump up, now certain of your next steps. You find your clothes neatly folded by the door back inside. Quickly putting them on, grabbing everything, stuffing it in your backpack you pause only to write a note to Rhoda. You leave it where you found your clothes and silently head out the back door, the same way you had come in this afternoon.
+
+With your flashlight you manage to pick your way through the vacant lot filled with beer bottles and cans. Something catches your eye. A pink glass bead, that kind with letters on them. It is stuck in the mud. Slowly you lean over to look at it. Frowning, you pull it from the mud. It is connected to a string or something caught on a vine. You have to yank it hard to release it. It’s part of a bracelet….no an anklet. Tears spring into your eyes. L-A-R-K-I-N. You and your friends had made these ankle bracelets when you were freshmen. You brush the mud from the letters tenderly. “Oh, Larkin, I would have talked you out of this and you knew it…”
+
+You sigh. Then you run all the way to Larkin’s house. And tomorrow you will go home.''',"\033[0m")
+done = input("So you finnaly make the story enter yes to end the game: ")
+if done =="yes":
+    print("It, is time to tell the story to the prince and save the butcher from the prison")
+prince = input(" So you here to tell me the story to reliase your friend?: ")
+print("okay tell me your story")
+prince2 = input("I like your story, i will reliase your friend!")
+print(name,"Thank you!")
+
+print("\033[32m",'''
+░██████╗░░█████╗░███╗░░░███╗███████╗  ███████╗███╗░░██╗██████╗░███████╗██████╗░
+██╔════╝░██╔══██╗████╗░████║██╔════╝  ██╔════╝████╗░██║██╔══██╗██╔════╝██╔══██╗
+██║░░██╗░███████║██╔████╔██║█████╗░░  █████╗░░██╔██╗██║██║░░██║█████╗░░██║░░██║
+██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░  ██╔══╝░░██║╚████║██║░░██║██╔══╝░░██║░░██║
+╚██████╔╝██║░░██║██║░╚═╝░██║███████╗  ███████╗██║░╚███║██████╔╝███████╗██████╔╝
+░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ╚══════╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═════╝░''',"\033[0m")
+
+print("Hope you like it!")
+
